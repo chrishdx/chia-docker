b6c726b436bff9b1ad42884b35c83b700cfdc2fd
chrishdx <50511550+chrishdx@users.noreply.github.com>
2021-09-12 15:27:21 +0200

    Update reboot.py

--- reboot.py
+++ reboot.py
@@ -1,1 +1,22 @@
-
+#!/usr/bin/python3
+import os
+devices = os.popen('chia plots show').readlines()
+USB = []
+Chia = []
+print("Script wird gestartet")
+for line in os.listdir("/usb"):
+ USB.append("/usb/" + line)
+for line in devices:
+ if "/" in line:
+  Chia.append(line.replace("\n",""))
+# hinzufuegen
+for line in USB:
+ if line not in Chia:
+  print("chia plots remove -d " + line)
+  os.system("chia plots add -d " + line)
+# entfernen
+for line in Chia:
+ if line not in USB and "txt" not in line:
+  print("chia plots remove -d " + line)
+  os.system("chia plots remove -d " + line)
+print("Script beendet")
